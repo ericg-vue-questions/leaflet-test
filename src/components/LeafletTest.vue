2524a30324c5d535a7d53c288bffe918b036da5b
ericg <ericgorr@gmail.com>
2022-06-29 19:50:19 -0400

    leaflet not working

---
+++ src/components/LeafletTest.vue
@@ -0,0 +1,54 @@
+<template>
+  <div id="container">
+    <div id="mapContainer"></div>
+  </div>
+</template>
+
+<script>
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+//
+// obtain a mapbox token from https://account.mapbox.com/access-tokens/
+// and then create token.js in ./src/components with the following content:
+//
+//    const token = "<your token>";
+//    export { token };
+//
+import {token} from "./token.js";
+
+export default {
+  name: "Map",
+
+  data() {
+    return{
+      center: [37,7749, -122,4194]
+    }
+  },
+
+  methods: {
+    setupLeafletMap: function () {
+      const mapDiv = L.map("mapContainer").setView(this.center, 13);
+      L.tileLayer(
+        `https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=${token}`,
+        {
+          attribution: 'Map data (c) <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+          maxZoom: 18,
+          id: "mapbox/streets-v11",
+          accessToken: `${token}`,
+        }
+      ).addTo(mapDiv);
+    },
+  },
+
+  mounted() {
+    this.setupLeafletMap();
+  },
+};
+</script>
+
+<style scoped>
+  #mapContainer {
+    width: 500px;
+    height: 500px;
+  }
+</style>
