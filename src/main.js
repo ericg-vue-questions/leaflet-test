5778a258ae47d20729ce129d0f7fe39883bae2bb
ericg <ericgorr@gmail.com>
2022-07-05 17:36:53 -0400

    [feature/lasso] add prettier support; add leaflet-lasso

--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
-require( "../node_modules/leaflet-polylinedecorator/dist/leaflet.polylineDecorator.js" );
+require('../node_modules/leaflet-polylinedecorator/dist/leaflet.polylineDecorator.js');
+require('../node_modules/leaflet-lasso/dist/leaflet-lasso.umd.min.js');
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+    render: (h) => h(App)
+}).$mount('#app');
