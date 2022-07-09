37f860c5d284d142a9c09a4180aaff99c0ef0518
ericg <ericgorr@gmail.com>
2022-07-09 16:35:50 -0400

    [feature/simple-marker] better answer to get the default icon working

--- src/components/LeafletTest.vue
+++ src/components/LeafletTest.vue
@@ -29,14 +29,6 @@ export default {
     },
 
     async mounted() {
-        delete L.Icon.Default.prototype._getIconUrl;
-
-        L.Icon.Default.mergeOptions({
-            iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
-            iconUrl: require('leaflet/dist/images/marker-icon.png'),
-            shadowUrl: require('leaflet/dist/images/marker-shadow.png')
-        });
-
         await Vue.nextTick();
         await this.setupLeafletMap();
     }
--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import { Icon } from 'leaflet';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+delete Icon.Default.prototype._getIconUrl;
+
+Icon.Default.mergeOptions({
+    iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+    iconUrl: require('leaflet/dist/images/marker-icon.png'),
+    shadowUrl: require('leaflet/dist/images/marker-shadow.png')
+});
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+    render: (h) => h(App)
+}).$mount('#app');
